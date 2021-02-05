dd4d760974d937f5824c68e14aaa3785df7f6438
jdj.souza@gmail.com <jdj.souza@gmail.com>
2021-02-05 11:03:35 -0600

    Created db, inserted data, added sql query to get data, rendered data on page

--- Controllers/GadgetsController.cs
+++ Controllers/GadgetsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JamesBondGadgetCollection.Data;
+using JamesBondGadgetCollection.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,14 @@ namespace JamesBondGadgetCollection.Controllers
         // GET: Gadgets
         public ActionResult Index()
         {
-            return View();
+            // generate some fake data and send it to a view
+            List<GadgetModel> gadgets = new List<GadgetModel>();
+
+            GadgetDAO gadgetDAO = new GadgetDAO();
+
+            gadgets = gadgetDAO.FetchAll();
+
+            return View("Index", gadgets);
         }
     }
 }
---
+++ Data/GadgetDAO.cs
@@ -0,0 +1,49 @@
+﻿using JamesBondGadgetCollection.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace JamesBondGadgetCollection.Data
+{
+    internal class GadgetDAO
+    {
+        private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BondGadgetDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        // performs all operations on the database. Get all, create, delete, get one, search etc.
+
+        public List<GadgetModel> FetchAll()
+        {
+            List<GadgetModel> returnList = new List<GadgetModel>();
+
+            // access the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * from [dbo].[Gadgets]";
+
+                SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // create a new gadget object. Add it to the list to return.
+                        GadgetModel gadget = new GadgetModel();
+                        gadget.Id = reader.GetInt32(0);
+                        gadget.Name = reader.GetString(1);
+                        gadget.Description = reader.GetString(2);
+                        gadget.AppearsIn = reader.GetString(3);
+                        gadget.WithThisActor = reader.GetString(4);
+
+                        returnList.Add(gadget);
+                    }
+                }
+
+
+
+            }
+
+                return returnList;
+        }
+    }
+}
