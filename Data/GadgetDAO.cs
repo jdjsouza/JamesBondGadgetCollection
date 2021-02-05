b5b4122dade92f685712cbb98a90d075117317a2
jdj.souza@gmail.com <jdj.souza@gmail.com>
2021-02-05 11:43:01 -0600

    Added a readme

--- Data/GadgetDAO.cs
+++ Data/GadgetDAO.cs
@@ -85,7 +85,59 @@ namespace JamesBondGadgetCollection.Data
 
         }
 
+        // create new
 
+        public int Create(GadgetModel gadgetModel)
+        {
+            // access the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "INSERT INTO dbo.Gadgets VALUES (@Name, @Description, @AppearsIn, @WithThisActor)";
+
+                // associate @id with Id parameter
+
+                SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                command.Parameters.Add("@Name", System.Data.SqlDbType.VarChar, 1000).Value = gadgetModel.Name;
+                command.Parameters.Add("@Description", System.Data.SqlDbType.VarChar, 1000).Value = gadgetModel.Description;
+                command.Parameters.Add("@AppearsIn", System.Data.SqlDbType.VarChar, 1000).Value = gadgetModel.AppearsIn;
+                command.Parameters.Add("@WithThisActor", System.Data.SqlDbType.VarChar, 1000).Value = gadgetModel.WithThisActor;
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                GadgetModel gadget = new GadgetModel();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // create a new gadget object. Add it to the list to return.
+
+                        gadget.Id = reader.GetInt32(0);
+                        gadget.Name = reader.GetString(1);
+                        gadget.Description = reader.GetString(2);
+                        gadget.AppearsIn = reader.GetString(3);
+                        gadget.WithThisActor = reader.GetString(4);
+
+
+                    }
+                }
+                return gadget;
+            }
+
+        }
+
+        // delete one
+
+
+        // update one
+
+
+        // search for name
+
+
+        // search for description
 
     }
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# James Bond Gadget Collection
+
+The James Bond Gadget Collection is a simple webapp that uses a database containing a list of gadgets the different James Bond's have used over the years.
+
+The goal of this project is to get more familiar with:
+  - ASP.NET
+  - C#
+  - VisualStudio 2019
